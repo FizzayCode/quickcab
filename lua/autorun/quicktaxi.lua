20594d52ae2b07efd27a58b8a1791c66fb6ac961
FizzayCode <fizzaycode@gmail.com>
2017-08-05 20:51:18 +0100

    Added autorun

---
+++ lua/autorun/quicktaxi.lua
@@ -0,0 +1,12 @@
+--[[
+			QuickTaxi
+	Autorun code
+
+	Do not edit without the permission from JustJosh
+]]--
+
+if SERVER then
+	include("quicktaxi/sv_init.lua");
+else
+	include("quicktaxi/cl_init.lua");
+end
