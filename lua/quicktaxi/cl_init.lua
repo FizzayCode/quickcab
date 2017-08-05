28a2f86ea2ca43b7f072c4906a42c0894bcc035b
FizzayCode <fizzaycode@gmail.com>
2017-08-05 20:49:56 +0100

    Initial Files

---
+++ lua/quicktaxi/cl_init.lua
@@ -0,0 +1,8 @@
+--[[
+			QuickTaxi
+	Clientside initialization code
+
+	Do not edit without the permission from JustJosh
+]]--
+
+if SERVER then return end
---
+++ lua/quicktaxi/sv_init.lua
@@ -0,0 +1,8 @@
+--[[
+			QuickTaxi
+	Serverside initialization code
+
+	Do not edit without the permission from JustJosh
+]]--
+
+if CLIENT then return end
